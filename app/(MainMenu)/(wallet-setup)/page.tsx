ec420096514b6c45517af8e41309e53d84289685
Jai Kumar <116871953+JaiKumarSuther@users.noreply.github.com>
2025-06-12 12:47:17 +0500

    fixed something

--- app/(MainMenu)/(wallet-setup)/page.tsx
+++ app/(MainMenu)/(wallet-setup)/page.tsx
@@ -2,12 +2,9 @@
 "use client";
 
 import React from "react";
-import DashboardLayout from "@/components/ui/DashboardLayout";
 
 export default function WalletSetupPage() {
   return (
-    <DashboardLayout title="Wallet Setup">
       <div>Your Wallet Setup Content</div>
-    </DashboardLayout>
   );
 }
