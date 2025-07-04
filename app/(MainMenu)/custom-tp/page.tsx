5c26f570b712c1741f65203f94a9cb3689e374b6
Jai Kumar <116871953+JaiKumarSuther@users.noreply.github.com>
2025-07-04 15:50:33 +0500

    fixed custom tp

--- app/(MainMenu)/custom-tp/page.tsx
+++ app/(MainMenu)/custom-tp/page.tsx
@@ -99,18 +99,18 @@ export default function CustomTP() {
   };
 
   return (
-    <div className="p-6 bg-[#000000] min-h-screen">
+    <div className=" bg-[#000000] min-h-screen">
       {/* Back Button */}
       <button
         onClick={handleBack}
-        className="text-white mb-10 cursor-pointer hover:text-[#8761FF] flex items-center gap-2"
+        className="text-white cursor-pointer hover:text-[#8761FF] flex items-center gap-2"
       >
         <ArrowLeft size={28} />
         <span className="text-base sm:text-2xl font-medium">Custom TP</span>
       </button>
 
       {/* Matrix Cards */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-2">
         <MatrixCard
           icon="/assets/wallet-card.svg"
           label="Total Wallets Configured"
@@ -129,110 +129,118 @@ export default function CustomTP() {
       </div>
 
       {/* Wallet Management Section */}
-      <div className="mt-12">
+      <div className="mt-4">
         <p className="text-white text-2xl font-semibold mb-4">
           Wallet Management
         </p>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-          {wallets.map((wallet) => (
-            <div
-              key={wallet.id}
-              className="p-4 rounded-lg bg-[#0D0E12] border border-[#161616] flex flex-col gap-4"
-            >
-              {/* Wallet Header */}
-              <div className="flex items-center justify-between gap-2">
-                <span className="text-white text-xl">Wallet</span>
-                <div className="flex items-center gap-3">
-                  <button className="text-[#8761FF] cursor-pointer text-xs flex items-center gap-1">
-                    <Image
-                      src="/assets/edit.svg"
-                      width={20}
-                      height={20}
-                      alt="wallet"
-                    />
-                    <span>Edit</span>
-                  </button>
-                  <button className="text-xs text-[#8761FF] flex items-center cursor-pointer gap-1">
-                    <Image
-                      src="/assets/remove.svg"
-                      width={14}
-                      height={14}
-                      alt="wallet"
-                    />
-                    <span>Delete</span>
-                  </button>
-                </div>
-              </div>
-
-              {/* Wallet Info Grid */}
-              <div className="grid grid-cols-2 gap-y-1">
-                <p className="text-[#B4B4B4] text-sm">Wallet ID</p>
-                <p className="text-sm text-[#929292] text-right">{wallet.id}</p>
-
-                <p className="text-[#B4B4B4] text-sm">Balance (SOL)</p>
-                <p className="text-sm text-[#929292] text-right">
-                  {wallet.balance}
-                </p>
-
-                <p className="text-[#B4B4B4] text-sm">MCAP</p>
-                <p className="text-sm text-[#929292] text-right">{wallet.MCAP}</p>
-
-                <p className="text-[#B4B4B4] text-sm">%</p>
-                <p className="text-sm text-[#929292] text-right">
-                  {wallet.percent}
-                </p>
-
-                <p className="text-[#B4B4B4] text-sm">Mode</p>
-                <div className="relative w-[100px] ml-auto">
-                  <div
-                    onClick={() => toggleDropdown(wallet.id)}
-                    className="cursor-pointer text-sm text-[#6A7A8C] pr-10 rounded-full py-1 bg-[#6E6E6E12] flex items-center justify-between pl-4"
-                  >
-                    <span>{wallet.mode}</span>
-                    {openDropdowns[wallet.id] ? (
-                      <ChevronUp size={16} className="absolute right-2" />
-                    ) : (
-                      <ChevronDown size={16} className="absolute right-2" />
-                    )}
+        <div className="w-full pr-1">
+          <div className="max-h-[400px] overflow-y-auto custom-scroll pr-2 mb-15">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+              {wallets.map((wallet) => (
+                <div
+                  key={wallet.id}
+                  className="p-4 rounded-lg bg-[#0D0E12] border border-[#161616] flex flex-col gap-4"
+                >
+                  {/* Wallet Header */}
+                  <div className="flex items-center justify-between gap-2">
+                    <span className="text-white text-xl">Wallet</span>
+                    <div className="flex items-center gap-3">
+                      <button className="text-[#8761FF] cursor-pointer text-xs flex items-center gap-1">
+                        <Image
+                          src="/assets/edit.svg"
+                          width={20}
+                          height={20}
+                          alt="wallet"
+                        />
+                        <span>Edit</span>
+                      </button>
+                      <button className="text-xs text-[#8761FF] flex items-center cursor-pointer gap-1">
+                        <Image
+                          src="/assets/remove.svg"
+                          width={14}
+                          height={14}
+                          alt="wallet"
+                        />
+                        <span>Delete</span>
+                      </button>
+                    </div>
                   </div>
 
-                  {openDropdowns[wallet.id] && (
-                    <div className="absolute top-full left-0 right-0 mt-1 bg-[#1A1B20] border border-[#22242D] rounded-md z-50 shadow-lg">
-                      {["Custom", "Standard", "Advanced"].map((mode) => (
-                        <div
-                          key={mode}
-                          onClick={() => handleModeChange(wallet.id, mode)}
-                          className="px-4 py-2 text-[#929292] text-sm cursor-pointer hover:bg-[#2A2B30] first:rounded-t-md last:rounded-b-md"
-                        >
-                          {mode}
-                        </div>
-                      ))}
-                    </div>
-                  )}
-                </div>
-              </div>
+                  {/* Wallet Info Grid */}
+                  <div className="grid grid-cols-2 gap-y-1">
+                    <p className="text-[#B4B4B4] text-sm">Wallet ID</p>
+                    <p className="text-sm text-[#929292] text-right">
+                      {wallet.id}
+                    </p>
 
-              {/* Action Buttons */}
-              <div className="flex justify-between gap-2 mt-2">
-                {[25, 50, 100].map((value) => (
-                  <button
-                    key={`green-${value}`}
-                    className="flex-1 py-1 text-xs rounded-md bg-[#1A1B20] border border-[#22242D] text-green-500 hover:bg-[#0f2a1b]/40 transition"
-                  >
-                    {value}
-                  </button>
-                ))}
-                {[25, 50, 100].map((value) => (
-                  <button
-                    key={`red-${value}`}
-                    className="flex-1 py-1 text-xs rounded-md bg-[#1A1B20] border border-[#22242D] text-red-500 hover:bg-[#2a0f0f]/40 transition"
-                  >
-                    {value}
-                  </button>
-                ))}
-              </div>
+                    <p className="text-[#B4B4B4] text-sm">Balance (SOL)</p>
+                    <p className="text-sm text-[#929292] text-right">
+                      {wallet.balance}
+                    </p>
+
+                    <p className="text-[#B4B4B4] text-sm">MCAP</p>
+                    <p className="text-sm text-[#929292] text-right">
+                      {wallet.MCAP}
+                    </p>
+
+                    <p className="text-[#B4B4B4] text-sm">%</p>
+                    <p className="text-sm text-[#929292] text-right">
+                      {wallet.percent}
+                    </p>
+
+                    <p className="text-[#B4B4B4] text-sm">Mode</p>
+                    <div className="relative w-[100px] ml-auto">
+                      <div
+                        onClick={() => toggleDropdown(wallet.id)}
+                        className="cursor-pointer text-sm text-[#6A7A8C] pr-10 rounded-full py-1 bg-[#6E6E6E12] flex items-center justify-between pl-4"
+                      >
+                        <span>{wallet.mode}</span>
+                        {openDropdowns[wallet.id] ? (
+                          <ChevronUp size={16} className="absolute right-2" />
+                        ) : (
+                          <ChevronDown size={16} className="absolute right-2" />
+                        )}
+                      </div>
+
+                      {openDropdowns[wallet.id] && (
+                        <div className="absolute top-full left-0 right-0 mt-1 bg-[#1A1B20] border border-[#22242D] rounded-md z-50 shadow-lg">
+                          {["Custom", "Standard", "Advanced"].map((mode) => (
+                            <div
+                              key={mode}
+                              onClick={() => handleModeChange(wallet.id, mode)}
+                              className="px-4 py-2 text-[#929292] text-sm cursor-pointer hover:bg-[#2A2B30] first:rounded-t-md last:rounded-b-md"
+                            >
+                              {mode}
+                            </div>
+                          ))}
+                        </div>
+                      )}
+                    </div>
+                  </div>
+
+                  {/* Action Buttons */}
+                  <div className="flex justify-between gap-2 mt-2">
+                    {[25, 50, 100].map((value) => (
+                      <button
+                        key={`green-${value}`}
+                        className="flex-1 py-1 text-xs rounded-md bg-[#1A1B20] border border-[#22242D] text-green-500 hover:bg-[#0f2a1b]/40 transition"
+                      >
+                        {value}
+                      </button>
+                    ))}
+                    {[25, 50, 100].map((value) => (
+                      <button
+                        key={`red-${value}`}
+                        className="flex-1 py-1 text-xs rounded-md bg-[#1A1B20] border border-[#22242D] text-red-500 hover:bg-[#2a0f0f]/40 transition"
+                      >
+                        {value}
+                      </button>
+                    ))}
+                  </div>
+                </div>
+              ))}
             </div>
-          ))}
+          </div>
         </div>
       </div>
     </div>
